66888dc3ce1ebadac60670cd30bbc6e0e065891d
Eduardo Silva <eduardosg.my@gmail.com>
2025-08-13 01:40:44 -0300

    feat: adiciona mais elementos à HomeView, posts são exibidos no perfil

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -25,12 +25,33 @@ defineProps({
         type: String,
         default: null,
     },
+    small: {
+        type: Boolean,
+        default: false,
+    }
 })
 
 </script>
 
 <template>
-    <section class="flex flex-wrap flex-col justify-start p-4 gap-1 w-[85vw] h-[60vh] bg-white font-mono text-[15px]/6">
+
+    <section v-if="small" class="flex flex-wrap flex-col justify-start p-1 w-[20vw] h-[12vh] bg-white font-mono">
+        <div class="flex justify-center items-center w-full max-h-[28vh]">
+            <img class="max-h-[5vh]" :src="image">
+        </div>
+        <div class="flex justify-between w-full items-center">
+            <div class="text-gray-500 italic text-[8px]">{{ date }}</div>
+        </div>
+        <div class="break-all text-[5.5px] wrap-anywhere">
+            {{ text }}
+        </div>
+        <div class="flex grow justify-between items-end">
+            <div class="text-gray-500 italic text-[5px]">{{ mood }}</div>
+            <div class="text-gray-500 italic text-[5px]">{{ friends }}</div>
+        </div>
+    </section>
+
+    <section v-else class="flex flex-wrap flex-col justify-start p-4 gap-1 w-[85vw] h-[60vh] bg-white font-mono text-[15px]/6">
         <div class="flex justify-center w-full max-h-[28vh]">
             <img :src="image">
         </div>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,10 +3,13 @@ import Navbar from '@/components/Navbar.vue';
 import PocketBase from 'pocketbase';
 import { onBeforeMount, onMounted, ref } from 'vue';
 import { RouterLink, useRouter } from 'vue-router';
+import Card from '@/components/Card.vue';
 
 let pb = null;
 const router = useRouter();
 const name = ref('');
+const avatar = ref('');
+const cards = ref([]);
 
 const logout = () => {
   pb.authStore.clear();
@@ -19,14 +22,40 @@ onBeforeMount(() => {
   if(!pb.authStore.isValid) router.replace("/login");
 })
 
-onMounted(() => {
-  name.value = pb.authStore.isValid ? pb.authStore.record.name : null;
+onMounted(async () => {
+  
+  await pb.collection('users').authRefresh();
+
+  name.value = pb.authStore.model.name;
+  avatar.value = pb.files.getURL(pb.authStore.model, pb.authStore.model.avatar);
+
+  cards.value = await pb.collection('cards').getFullList({filter: `idUser="${pb.authStore.record.id}"`});
+  console.log(cards);
 })
 </script>
 
 <template>
-  <h2>Welcome {{ name }}</h2>
-  <button @click="logout" class="bg-red-700 rounded">Logout</button>
-  <RouterLink to="/friends" class="bg-green-700 rounded">Friends</RouterLink>
+  <section class="flex flex-col w-full">
+
+      <div class="flex flex-col flex-wrap w-[100vw] h-[35vh] bg-white rounded-b-[10vw] gap-x-[2vw] gap-y-[5vh]">
+        <div class="flex gap-x-[3vw] m-[1vh] justify-end">
+          <RouterLink to="/friends" class="p-[1.5vw] rounded-full bg-green-700 rounded text-white font-bold">Friends</RouterLink>
+          <button @click="logout" class="p-[1.5vw] rounded-full bg-red-700 rounded text-white font-bold">Logout</button>
+        </div>
+        <div class="flex ml-[5vw] gap-5 items-center m-5">
+          <img :src="avatar" class="aspect-square rounded-full w-[25vw]"></img>
+          <div class="flex flex-col">
+            <div class="font-bold text-[20px]">{{ name }}</div>
+            <div class="text-[14px]">Olá! Essa é a minha descrição completamente aleatória.</div>
+          </div>
+        </div>
+      </div>
+      <section class="m-5 flex flex-wrap justify-start gap-2">
+        <div v-for="card in cards">
+          <Card :image="pb.files.getURL(card, card.image)" :text="card.text.substring(0, 20) + '...'" :date="card.date" :small="true" />
+        </div>
+      </section>
+  </section>
+
   <Navbar/>
 </template>
