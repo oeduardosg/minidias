fd0ea3347fb79680802dfcc6f95c2c43e16131ae
Eduardo Silva <eduardosg.my@gmail.com>
2025-08-13 11:50:04 -0300

    feat: posts são clicáveis e ficam grandes na tela

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -32,11 +32,38 @@ onMounted(async () => {
   cards.value = await pb.collection('cards').getFullList({filter: `idUser="${pb.authStore.record.id}"`});
   console.log(cards);
 })
+
+const showingImage = ref('');
+const showingText = ref('');
+const showingLocal = ref('');
+const showingDate = ref('');
+const showingMood = ref('');
+const showingFriends = ref('');
+const isShowing = ref(false);
+
+function showCard(card) {
+  showingImage.value = pb.files.getURL(card, card.image);
+  showingText.value = card.text;
+  showingLocal.value = card.local;
+  showingDate.value = card.date;
+  showingMood.value = card.mood;
+  showingFriends.value = card.friends;
+  isShowing.value = true;
+  console.log(card);
+}
+
+function showOff() {
+  isShowing.value = false;
+}
+
 </script>
 
 <template>
-  <section class="flex flex-col w-full">
+  <section class="flex flex-col w-full relative">
 
+      <div @click="showOff()" class="absolute w-full h-screen flex justify-center items-center backdrop-brightness-50" v-if="isShowing">
+        <Card @click.stop :image="showingImage" :date="showingDate" :text="showingText" :local="showingLocal" :mood="showingMood" :friends="showingFriends"/>
+      </div>
       <div class="flex flex-col flex-wrap w-[100vw] h-[35vh] bg-white rounded-b-[10vw] gap-x-[2vw] gap-y-[5vh]">
         <div class="flex gap-x-[3vw] m-[1vh] justify-end">
           <RouterLink to="/friends" class="p-[1.5vw] rounded-full bg-green-700 rounded text-white font-bold">Friends</RouterLink>
@@ -52,10 +79,11 @@ onMounted(async () => {
       </div>
       <section class="m-5 flex flex-wrap justify-start gap-2">
         <div v-for="card in cards">
-          <Card :image="pb.files.getURL(card, card.image)" :text="card.text.substring(0, 20) + '...'" :date="card.date" :small="true" />
+          <Card class="cursor-pointer" @click="showCard(card)" :image="pb.files.getURL(card, card.image)" :text="card.text.substring(0, 20) + '...'" :date="card.date" :small="true" />
         </div>
       </section>
   </section>
 
-  <Navbar/>
+  <Navbar v-if="isShowing" @click.stop class="brightness-50"/>
+  <Navbar v-else/>
 </template>
